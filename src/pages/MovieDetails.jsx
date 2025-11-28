7cdf1064240d09c209de55c6770f329a58420a2b
Alkis Karagiannis <alkis.k1972@gmail.com>
2025-11-28 16:11:03 +0200

    More of MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -11,6 +11,7 @@ const MovieDetails = () => {
         overview,
         vote_average,
         poster_path,
+        backdrop_path,
     } = movie;
 
     const ratingValue = vote_average?.toFixed(1);
@@ -18,6 +19,29 @@ const MovieDetails = () => {
     const posterURL = poster_path
         ? `https://image.tmdb.org/t/p/w500${poster_path}`
         : 'https://placehold.co/500x750/orange/orange';
+    const backdropURL = `https://image.tmdb.org/t/p/original${backdrop_path}`;
+
+    const filmGenres = {
+        28: 'Action',
+        12: 'Adventure',
+        16: 'Animation',
+        35: 'Comedy',
+        80: 'Crime',
+        99: 'Documentary',
+        18: 'Drama',
+        10751: 'Family',
+        14: 'Fantasy',
+        36: 'History',
+        27: 'Horror',
+        10402: 'Music',
+        9648: 'Mystery',
+        10749: 'Romance',
+        878: 'Sci-Fi',
+        10770: 'TV Movie',
+        53: 'Thriller',
+        10752: 'War',
+        37: 'Western',
+    };
 
     if (!movie) {
         return <Navigate to='*' />;
@@ -26,31 +50,44 @@ const MovieDetails = () => {
     }
 
     return (
-        <div className='mx-auto max-w-7xl min-h-screen flex flex-col items-center'>
-            <div>
-                <Link to='/'>
-                    ⬅️ <span>Back to List</span>
-                </Link>
+        <>
+            <div
+                className='h-64 w-full bg-cover bg-center opacity-40'
+                style={{ backgroundImage: `url(${backdropURL})` }}
+            >
+                {' '}
             </div>
-            <div className='flex flex-col justify-start items-start'>
-                <p>
-                    <img src={posterURL} alt={title} />
-                </p>
-                <p>
-                    {title} - {year}
-                </p>
-
-                <p>
-                    Genres:{' '}
-                    {genre_ids.map((genre, index) => (
-                        <span key={index}>{genre} </span>
-                    ))}
-                </p>
-                <p>Overview: {overview}</p>
-                <p>Rating: {ratingValue}</p>
-                <p>movie ID: {id}</p>
+            <div className='max-w-6xl mx-auto -mt-24'>
+                <div>
+                    <Link to='/'>
+                        ⬅️ <span>Back to List</span>
+                    </Link>
+                </div>
+                <div className='grid gap-10 lg:grid-cols-3'>
+                    <div className='relative aspect-[2/3] w-full rounded-xl overflow-hidden shadow-lg'>
+                        <img src={posterURL} alt={title} />
+                    </div>
+                    <div className='flex flex-col'>
+                        <h1 className='flex items-end gap-3'>
+                            <span className='text-4xl'>{title}</span>
+                            <span className='text-lg'>{year}</span>
+                        </h1>
+                        <div className='flex flex-col'>
+                            <p>
+                                {genre_ids
+                                    .map((id) => filmGenres[id])
+                                    .join(' • ')}
+                            </p>
+                            <p>{overview}</p>
+                            <p>movie ID: {id}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Rating: {ratingValue}</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
