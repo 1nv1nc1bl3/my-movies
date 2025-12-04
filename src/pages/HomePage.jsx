7a0685852495ab4183d9f6c08ade4c0f3c4c623c
Alkis Karagiannis <alkis.k1972@gmail.com>
2025-12-04 12:53:31 +0200

    SearchBar full width fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
         <div className='min-h-[calc(100vh-8rem)] bg-slate-50'>
             {/* SEARCH STRIP – μοναδικό search, κάτω από navbar */}
             <div className='w-full bg-white shadow-sm'>
-                <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 bg-teal-300'>
+                <div className='max-w-full mx-auto px-4 sm:px-6 lg:px-8 py-4 bg-teal-300'>
                     <SearchBar query={query} setQuery={setQuery} />
                 </div>
             </div>
